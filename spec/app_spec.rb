ad997cc922489857ec98e8836c48b03bf58c936c
okamuuu <okamuuu@gmail.com>
2014-08-28 09:40:02 +0900

    added test case.

---
+++ spec/app_spec.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
+require './app'
+require 'rspec'
+require 'rack/test'
+
+describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Hello, World!')
+  end
+end
