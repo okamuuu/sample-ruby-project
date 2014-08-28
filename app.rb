5f0a3478369cd4161fe7396063ac923ae72bcc99
okamuuu <okamuuu@gmail.com>
2014-08-28 09:46:33 +0900

    replace hello

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra';
+require 'groonga'
+require 'pathname'
 
+database_file = Pathname('groonga/database')
+database_directory = database_file.dirname
+database_directory.mkpath unless database_directory.exist?
+
+if database_file.exist?
+  Groonga::Database.open(database_file.to_s)
+else
+  Groonga::Database.create(:path => database_file.to_s)
+end
+
+if not Groonga["Items"]
+  Groonga::Schema.create_table("Items", :type => :hash)
+  items = Groonga["Items"]
+  items.add("hello")
+
+  Groonga::Schema.change_table("Items") do |table|
+    table.text("title")
+  end
+  items["hello"].title = "Hello, World!"
+end
+  
 get '/' do
-  'Hello, World!'
+  Groonga["Items"]["hello"].title
+  #'Hello, World!'
 end
